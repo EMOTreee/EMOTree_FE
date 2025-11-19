06327bb554d8414075ad237039d64069b8259abf
jjjuni <0210leejun@gmail.com>
2025-11-20 00:14:03 +0900

    ✨ Feat: 레이아웃 및 라우팅 설정

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/layout/Header";
+
+export default function Layout() {
+  return (
+    <div className="bg-bg flex min-h-screen flex-col">
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  )
+}
