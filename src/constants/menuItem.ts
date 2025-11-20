2e1d87f2e4475b2a047b4aea0622cbf1897b12b1
jjjuni <0210leejun@gmail.com>
2025-11-21 06:41:09 +0900

    ♻️ Refactor: 상수 수정

--- src/constants/menuItem.ts
+++ src/constants/menuItem.ts
@@ -1,25 +1,31 @@
 export const items = [
   {
-    image: '/MainLogo.svg',
-    link: 'https://google.com/',
-    title: 'EMOTree',
-    description: '감정을 읽고 공감하는 능력, EMOTree에서 자연스럽게 성장시켜 보세요. 표정과 목소리의 작은 신호를 이해하며 일상 속 감정을 조금 더 선명하게 바라볼 수 있습니다.'
+    image: '/images/purple.png',
+    link: '/express/expression',
+    title: '감정 표현 트레이닝',
+    description: '내 감정을 솔직하면서도 자연스럽게 전하는 연습을 통해, 편안한 소통의 리듬을 만들어 보세요. EMOTree는 당신이 자신만의 감정 언어를 찾도록 도와줍니다.'
   },
   {
-    image: 'https://picsum.photos/400/400?grayscale',
-    link: 'https://google.com/',
-    title: '감정 해석 트레이닝',
-    description: '표정과 목소리에 담긴 미묘한 신호를 더 섬세하게 읽어내며, 감정의 흐름을 차분히 이해하는 능력을 키워보세요. EMOTree는 이러한 작은 감정의 흔적을 명확하게 바라볼 수 있도록 도와줍니다.'
-  },
-  {
-    image: 'https://picsum.photos/500/500?grayscale',
-    link: 'https://google.com/',
+    image: '/images/orange.png',
+    link: '/emotion/empathy',
     title: '감정 공감 트레이닝',
     description: '타인의 감정을 있는 그대로 받아들이고, 그 마음의 온도를 함께 느끼는 공감 경험을 쌓아보세요. EMOTree와 함께라면 일상 속 관계가 한층 더 부드러워집니다.'
   },
   {
-    image: 'https://picsum.photos/600/600?grayscale',
-    link: 'https://google.com/',
+    image: '/images/yellow.png',
+    link: '/emotion/interpret',
+    title: '감정 해석 트레이닝',
+    description: '표정과 목소리에 담긴 미묘한 신호를 더 섬세하게 읽어내며, 감정의 흐름을 차분히 이해하는 능력을 키워보세요. EMOTree는 이러한 작은 감정의 흔적을 명확하게 바라볼 수 있도록 도와줍니다.'
+  },
+  {
+    image: '/images/EMOTree.png',
+    link: '/',
+    title: 'EMOTree',
+    description: '감정을 읽고 공감하는 능력, EMOTree에서 자연스럽게 성장시켜 보세요. 표정과 목소리의 작은 신호를 이해하며 일상 속 감정을 조금 더 선명하게 바라볼 수 있습니다.'
+  },
+  {
+    image: '/images/green.png',
+    link: '/express/voice',
     title: '감정 표현 트레이닝',
     description: '내 감정을 솔직하면서도 자연스럽게 전하는 연습을 통해, 편안한 소통의 리듬을 만들어 보세요. EMOTree는 당신이 자신만의 감정 언어를 찾도록 도와줍니다.'
   }
