05c5a1c804abe717d8183ef43ba6a8aaaec4af1c
jjjuni <0210leejun@gmail.com>
2025-11-21 06:36:31 +0900

    💄Style: 아이콘 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,7 @@
-export { default as GreenIcon } from './header_icon.svg?react';
+export { default as GreenLogo } from './header_icon.svg?react';
+export { default as RandomIcon } from './random.svg?react';
+export { default as HappyIcon } from './happy.svg?react';
+export { default as SadIcon } from './sad.svg?react';
+export { default as AngryIcon } from './angry.svg?react';
+export { default as AnxietyIcon } from './anxiety.svg?react';
+export { default as SurpriseIcon } from './surprise.svg?react';
